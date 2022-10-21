90f9de2c7596a09c36b0446fdfce70f100c904b2
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-10-21 21:22:07 +0700

    fix booking
    fix booking at the same time

--- Source/Models/booking.js
+++ Source/Models/booking.js
@@ -1,26 +1,30 @@
-const { DataTypes } = require('sequelize');
-const db=require('./database');
-const User=require('./users');
-const Showtime=require('./showtime');
+const { DataTypes } = require("sequelize");
+const db = require("./database");
+const User = require("./users");
+const Showtime = require("./showtime");
 
-const Booking = db.define('Booking', {
+const Booking = db.define(
+  "Booking",
+  {
     // Model attributes are defined here
     ID: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-    }, 
-    Date:{
-        type: DataTypes.DATE,
-        allowNull: true,
-    },  
-    PriceTotal:{
-        type: DataTypes.DOUBLE,
-        allowNull: true,
-    },  
-  },{
-       // Other model options go here
-});
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+    },
+    Date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    PriceTotal: {
+      type: DataTypes.DOUBLE,
+      allowNull: true,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
 
 Booking.belongsTo(User);
 User.hasMany(Booking);
@@ -28,57 +32,65 @@ User.hasMany(Booking);
 Booking.belongsTo(Showtime);
 Showtime.hasMany(Booking);
 
-Booking.createBooking = async function(id,date,priceTotal,showtimeId,userId) {
-    await Booking.create({
-        ID: id,
-        Date: date,
-        PriceTotal: priceTotal,
-        ShowtimeId: showtimeId,
-        UserId: userId
-    });
-}
+Booking.createBooking = async function (
+  id,
+  date,
+  priceTotal,
+  userId,
+  showtimeId,
+  transaction
+) {
+  await Booking.create(
+    {
+      ID: id,
+      Date: date,
+      PriceTotal: priceTotal,
+      UserId: userId,
+      ShowtimeId: showtimeId,
+    },
+    { transaction: transaction }
+  );
+};
 
- Booking.getListBookingOfUser= async function(userId)
- {
-    //get date, movie, movietheater, tickets
-    const bookings = await db.query('SELECT "bk"."createdAt", "mv"."Name" AS "nameMovie", "mvt"."Name" AS "nameMovieTheater" FROM "Bookings" AS bk JOIN "Showtimes" AS st ON "bk"."ShowtimeId"="st"."id" JOIN "Movies" AS mv ON "mv"."id"="st"."MovieId" JOIN "MovieTheaters" AS mvt ON "mvt"."id"="st"."MovieTheaterId" WHERE "bk"."UserId"=?',
-                     {   replacements: [userId],
-                         type: db.QueryTypes.SELECT 
-                    });
-    return bookings;
- };
+Booking.getListBookingOfUser = async function (userId) {
+  //get date, movie, movietheater, tickets
+  const bookings = await db.query(
+    'SELECT "bk"."createdAt", "mv"."Name" AS "nameMovie", "mvt"."Name" AS "nameMovieTheater" FROM "Bookings" AS bk JOIN "Showtimes" AS st ON "bk"."ShowtimeId"="st"."id" JOIN "Movies" AS mv ON "mv"."id"="st"."MovieId" JOIN "MovieTheaters" AS mvt ON "mvt"."id"="st"."MovieTheaterId" WHERE "bk"."UserId"=?',
+    { replacements: [userId], type: db.QueryTypes.SELECT }
+  );
+  return bookings;
+};
 
+Booking.getTotalRevenueByMonth = async function () {
+  const total = await db.query(
+    'SELECT EXTRACT( MONTH FROM "bk"."Date") AS Thang, EXTRACT( YEAR FROM "bk"."Date") AS Nam, SUM("bk"."PriceTotal") AS DoanhThu FROM "Bookings" AS bk GROUP BY EXTRACT( MONTH FROM "bk"."Date"), EXTRACT( YEAR FROM "bk"."Date") ORDER BY EXTRACT( YEAR FROM "bk"."Date") DESC, EXTRACT( MONTH FROM "bk"."Date") DESC OFFSET 0 LIMIT 4',
+    { type: db.QueryTypes.SELECT }
+  );
+  return total;
+};
 
- Booking.getTotalRevenueByMonth = async function()
- {
-    const total = await db.query('SELECT EXTRACT( MONTH FROM "bk"."Date") AS Thang, EXTRACT( YEAR FROM "bk"."Date") AS Nam, SUM("bk"."PriceTotal") AS DoanhThu FROM "Bookings" AS bk GROUP BY EXTRACT( MONTH FROM "bk"."Date"), EXTRACT( YEAR FROM "bk"."Date") ORDER BY EXTRACT( YEAR FROM "bk"."Date") DESC, EXTRACT( MONTH FROM "bk"."Date") DESC OFFSET 0 LIMIT 4',
-                { type: db.QueryTypes.SELECT });
-    return total;
- }
+Booking.getTotalRevenueByDay = async function () {
+  const total = await db.query(
+    'SELECT EXTRACT(DAY FROM "bk"."Date") AS Ngay, EXTRACT( MONTH FROM "bk"."Date") AS Thang, EXTRACT( YEAR FROM "bk"."Date") AS Nam, SUM("bk"."PriceTotal") AS DoanhThu FROM "Bookings" AS bk GROUP BY EXTRACT(DAY FROM "bk"."Date"), EXTRACT( MONTH FROM "bk"."Date"), EXTRACT( YEAR FROM "bk"."Date") ORDER BY EXTRACT( YEAR FROM "bk"."Date") DESC, EXTRACT( MONTH FROM "bk"."Date") DESC OFFSET 0 LIMIT 4',
+    { type: db.QueryTypes.SELECT }
+  );
+  return total;
+};
 
- Booking.getTotalRevenueByDay = async function()
- {
-    const total = await db.query('SELECT EXTRACT(DAY FROM "bk"."Date") AS Ngay, EXTRACT( MONTH FROM "bk"."Date") AS Thang, EXTRACT( YEAR FROM "bk"."Date") AS Nam, SUM("bk"."PriceTotal") AS DoanhThu FROM "Bookings" AS bk GROUP BY EXTRACT(DAY FROM "bk"."Date"), EXTRACT( MONTH FROM "bk"."Date"), EXTRACT( YEAR FROM "bk"."Date") ORDER BY EXTRACT( YEAR FROM "bk"."Date") DESC, EXTRACT( MONTH FROM "bk"."Date") DESC OFFSET 0 LIMIT 4',
-                { type: db.QueryTypes.SELECT });
-    return total;
- }
+Booking.getRevenueByLocation = async function (start, end) {
+  const total = await db.query(
+    'SELECT "mvtc"."Name" AS namelocation, CASE WHEN SUM("bk"."PriceTotal") is NULL THEN 0 ELSE SUM(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN "bk"."PriceTotal" ELSE 0 END) END AS doanhthu, CASE WHEN COUNT("tk"."ID") is NULL THEN 0 ELSE COUNT(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN 1 ELSE NULL END) END AS numtickets FROM "Bookings" AS bk JOIN "Tickets" AS tk ON "tk"."BookingID"="bk"."ID" JOIN "Showtimes" AS st ON "bk"."ShowtimeId"="st"."id" JOIN "MovieTheaters" AS mvt ON "st"."MovieTheaterId"="mvt"."id" RIGHT JOIN "MovieTheaterClusters" AS mvtc ON "mvtc"."id"="mvt"."MovieTheaterClusterId" GROUP BY "mvtc"."id","mvtc"."Name" ORDER BY doanhthu DESC',
+    { replacements: [start, end, start, end], type: db.QueryTypes.SELECT }
+  );
+  return total;
+};
 
- Booking.getRevenueByLocation = async function(start,end)
- {
-    const total = await db.query('SELECT "mvtc"."Name" AS namelocation, CASE WHEN SUM("bk"."PriceTotal") is NULL THEN 0 ELSE SUM(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN "bk"."PriceTotal" ELSE 0 END) END AS doanhthu, CASE WHEN COUNT("tk"."ID") is NULL THEN 0 ELSE COUNT(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN 1 ELSE NULL END) END AS numtickets FROM "Bookings" AS bk JOIN "Tickets" AS tk ON "tk"."BookingID"="bk"."ID" JOIN "Showtimes" AS st ON "bk"."ShowtimeId"="st"."id" JOIN "MovieTheaters" AS mvt ON "st"."MovieTheaterId"="mvt"."id" RIGHT JOIN "MovieTheaterClusters" AS mvtc ON "mvtc"."id"="mvt"."MovieTheaterClusterId" GROUP BY "mvtc"."id","mvtc"."Name" ORDER BY doanhthu DESC',
-                {   replacements: [start,end,start,end],
-                    type: db.QueryTypes.SELECT 
-                });
-    return total;
- }
+Booking.getRevenueByMovie = async function (start, end) {
+  const total = await db.query(
+    'SELECT "mv"."Name" AS namemovie, CASE WHEN SUM("bk"."PriceTotal") is NULL THEN 0 ELSE SUM(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN "bk"."PriceTotal" ELSE 0 END) END AS doanhthu, CASE WHEN COUNT("tk"."ID") is NULL THEN 0 ELSE COUNT(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN 1 ELSE NULL END) END AS numtickets FROM "Bookings" AS bk JOIN "Tickets" AS tk ON "tk"."BookingID"="bk"."ID" JOIN "Showtimes" AS st ON "bk"."ShowtimeId"="st"."id" RIGHT JOIN "Movies" AS mv ON "st"."MovieId"="mv"."id" GROUP BY "mv"."id","mv"."Name" ORDER BY doanhthu DESC',
+    { replacements: [start, end, start, end], type: db.QueryTypes.SELECT }
+  );
+  return total;
+};
 
- Booking.getRevenueByMovie = async function(start,end)
- {
-    const total = await db.query('SELECT "mv"."Name" AS namemovie, CASE WHEN SUM("bk"."PriceTotal") is NULL THEN 0 ELSE SUM(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN "bk"."PriceTotal" ELSE 0 END) END AS doanhthu, CASE WHEN COUNT("tk"."ID") is NULL THEN 0 ELSE COUNT(CASE WHEN "bk"."Date">=? AND "bk"."Date"<=? THEN 1 ELSE NULL END) END AS numtickets FROM "Bookings" AS bk JOIN "Tickets" AS tk ON "tk"."BookingID"="bk"."ID" JOIN "Showtimes" AS st ON "bk"."ShowtimeId"="st"."id" RIGHT JOIN "Movies" AS mv ON "st"."MovieId"="mv"."id" GROUP BY "mv"."id","mv"."Name" ORDER BY doanhthu DESC',
-    {   replacements: [start,end,start,end],
-        type: db.QueryTypes.SELECT 
-    });
-    return total;
- }
- 
-module.exports=Booking;
+module.exports = Booking;
--- Source/Models/movieTheater.js
+++ Source/Models/movieTheater.js
@@ -1,89 +1,102 @@
-const { DataTypes } = require('sequelize');
-const db=require('./database');
-const mvThCluster=require('./movieTheaterCluster');
+const { DataTypes } = require("sequelize");
+const db = require("./database");
+const mvThCluster = require("./movieTheaterCluster");
 
-
-const MovieTheater = db.define('MovieTheater', {
+const MovieTheater = db.define(
+  "MovieTheater",
+  {
     // Model attributes are defined here
     Name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    }, 
-    Address:{
-        type: DataTypes.STRING,
-        allowNull: true,
+      type: DataTypes.STRING,
+      allowNull: true,
     },
-    Image:{
-        type: DataTypes.BLOB,
-        allowNull: true,
-    },  
-    Kind:{
-        type: DataTypes.STRING, // 2D, 3D,...
-        allowNull: true,
-    },  
-    Width:{
-        type: DataTypes.STRING,
-        allowNull: true,
-    },  
-    Height:{
-        type: DataTypes.STRING,
-        allowNull: true,
-    },  
-  },{
-       // Other model options go here
-  });
-
-  MovieTheater.createMovieTheater = async function(name,address,image,kind,width,height,movietheaterclusterId) {
-    await MovieTheater.create({
-             Name:name,
-             Address:address,
-             Image:image,
-             Kind:kind,
-             Width:width,
-             Height: height,
-             MovieTheaterClusterId:movietheaterclusterId,
-         });
- }
-MovieTheater.getAll = async function()
-{
-    return await MovieTheater.findAll();
-};
-MovieTheater.findById= async function(id)
-{
-    return await MovieTheater.findByPk(id);
-};
-
-MovieTheater.getByLocationId = async function(locationId)
-{
-    const cinemas = await db.query('SELECT * FROM "MovieTheaters" WHERE "MovieTheaterClusterId" = ?', 
-    { 
-        replacements: [locationId], //mảng danh sách tham số
-        type: db.QueryTypes.SELECT 
-    });
-    return cinemas;
-};
-
-MovieTheater.getListMovieTheater= async function(movietheaterId,pageIndex,pageSize)
-{
-    const offset=(pageIndex-1)*pageSize;
-    const movies = await db.query('SELECT * FROM "MovieTheaters" WHERE "MovieTheaterClusterId" = ? OFFSET ? LIMIT ?', 
-            { 
-                replacements: [movietheaterId,offset,pageSize], //mảng danh sách tham số
-                type: db.QueryTypes.SELECT 
-            });
-    return movies;
-};
-MovieTheater.deleteById = async function(id) {
-    await MovieTheater.destroy({
-        where: {
-          id
-        }
-    });
-};
-
+    Address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Image: {
+      type: DataTypes.BLOB,
+      allowNull: true,
+    },
+    Kind: {
+      type: DataTypes.STRING, // 2D, 3D,...
+      allowNull: true,
+    },
+    Width: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Height: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
 
 MovieTheater.belongsTo(mvThCluster);
 mvThCluster.hasMany(MovieTheater);
 
+MovieTheater.createMovieTheater = async function (
+  name,
+  address,
+  image,
+  kind,
+  width,
+  height,
+  movietheaterclusterId
+) {
+  await MovieTheater.create({
+    Name: name,
+    Address: address,
+    Image: image,
+    Kind: kind,
+    Width: width,
+    Height: height,
+    MovieTheaterClusterId: movietheaterclusterId,
+  });
+};
+MovieTheater.getAll = async function () {
+  return await MovieTheater.findAll();
+};
+MovieTheater.findById = async function (id) {
+  return await MovieTheater.findByPk(id);
+};
 
-module.exports=MovieTheater;
+MovieTheater.getByLocationId = async function (locationId) {
+  const cinemas = await db.query(
+    'SELECT * FROM "MovieTheaters" WHERE "MovieTheaterClusterId" = ?',
+    {
+      replacements: [locationId], //mảng danh sách tham số
+      type: db.QueryTypes.SELECT,
+    }
+  );
+  return cinemas;
+};
+
+MovieTheater.getListMovieTheater = async function (
+  movietheaterId,
+  pageIndex,
+  pageSize
+) {
+  const offset = (pageIndex - 1) * pageSize;
+  const movies = await db.query(
+    'SELECT * FROM "MovieTheaters" WHERE "MovieTheaterClusterId" = ? OFFSET ? LIMIT ?',
+    {
+      replacements: [movietheaterId, offset, pageSize], //mảng danh sách tham số
+      type: db.QueryTypes.SELECT,
+    }
+  );
+  return movies;
+};
+MovieTheater.deleteById = async function (id) {
+  await MovieTheater.destroy({
+    where: {
+      id,
+    },
+  });
+};
+
+module.exports = MovieTheater;
